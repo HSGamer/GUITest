57aa5a2934394fd62e71316eb086092d46469914
hsgamer <huynhqtienvtag@gmail.com>
2021-02-26 17:55:22 +0700

    update

--- src/main/java/me/hsgamer/guitest/GUITest.java
+++ src/main/java/me/hsgamer/guitest/GUITest.java
@@ -5,8 +5,8 @@ import me.hsgamer.hscore.bukkit.gui.advanced.GUIHolder;
 import me.hsgamer.hscore.bukkit.gui.advanced.GUIListener;
 import me.hsgamer.hscore.bukkit.gui.button.DummyButton;
 import me.hsgamer.hscore.bukkit.gui.button.SimpleButton;
-import me.hsgamer.hscore.bukkit.gui.mask.FillMask;
 import me.hsgamer.hscore.bukkit.gui.mask.Mask;
+import me.hsgamer.hscore.bukkit.gui.mask.RectangleMask;
 import me.hsgamer.hscore.bukkit.gui.mask.SingleMask;
 import me.hsgamer.hscore.common.Pair;
 import org.bukkit.Material;
@@ -53,18 +53,18 @@ public final class GUITest extends BasePlugin {
         mask1.init();
         Mask mask2 = new SingleMask("mask2", 1, new DummyButton(new ItemStack(Material.STONE)));
         mask2.init();
-        Mask mask3 = new FillMask("mask3", 2, 17, new DummyButton(new ItemStack(Material.REDSTONE)));
+        Mask mask3 = new RectangleMask("mask3", 2, 8, new DummyButton(new ItemStack(Material.REDSTONE)));
         mask3.init();
-        Mask mask4 = new FillMask("mask4", Pair.of(0, 1), Pair.of(8, 5), new DummyButton(new ItemStack(Material.DIAMOND)));
+        Mask mask4 = new RectangleMask("mask4", Pair.of(0, 1), Pair.of(8, 5), new DummyButton(new ItemStack(Material.DIAMOND)));
         mask4.init();
-        Mask mask5 = new FillMask("mask5", Pair.of(1, 2), Pair.of(7, 4), new SimpleButton(new ItemStack(Material.DIAMOND_SWORD), (uuid, event) -> {
+        Mask mask5 = new RectangleMask("mask5", Pair.of(1, 2), Pair.of(7, 4), new SimpleButton(new ItemStack(Material.DIAMOND_SWORD), (uuid, event) -> {
             getLogger().info(event.getWhoClicked().getName() + " clicked this diamond sword");
         }));
         mask5.init();
         holder.addMask(mask1);
         holder.addMask(mask2);
-        holder.addMask(mask4);
         holder.addMask(mask3);
+        holder.addMask(mask4);
         holder.addMask(mask5);
         holder.init();
         registerCommand(new Command("guitest") {
