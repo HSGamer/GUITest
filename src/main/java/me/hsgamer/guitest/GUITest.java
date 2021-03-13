35405bef9172e5101979d996e34e0f2b3c84aaab
hsgamer <huynhqtienvtag@gmail.com>
2021-03-13 23:39:03 +0700

    test PaginatedMask

--- src/main/java/me/hsgamer/guitest/GUITest.java
+++ src/main/java/me/hsgamer/guitest/GUITest.java
@@ -3,12 +3,11 @@ package me.hsgamer.guitest;
 import me.hsgamer.hscore.bukkit.baseplugin.BasePlugin;
 import me.hsgamer.hscore.bukkit.gui.GUIListener;
 import me.hsgamer.hscore.bukkit.gui.advanced.AdvancedGUIHolder;
-import me.hsgamer.hscore.bukkit.gui.button.*;
-import me.hsgamer.hscore.bukkit.gui.mask.Mask;
+import me.hsgamer.hscore.bukkit.gui.button.impl.SimpleButton;
 import me.hsgamer.hscore.bukkit.gui.mask.MaskUtils;
-import me.hsgamer.hscore.bukkit.gui.mask.impl.MultiSlotsMask;
-import me.hsgamer.hscore.bukkit.gui.mask.impl.MultiUserMultiSlotsMask;
+import me.hsgamer.hscore.bukkit.gui.mask.impl.PaginatedMask;
 import me.hsgamer.hscore.bukkit.gui.mask.impl.SingleMask;
+import me.hsgamer.hscore.ui.Position2D;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -52,24 +51,25 @@ public final class GUITest extends BasePlugin {
 
     private void initHolder() {
         holder.setSize(54);
-        Mask mask1 = new MultiSlotsMask("mask1", MaskUtils.generateOutlineSlots(0, 1, 8, 5).collect(ArrayList::new, ArrayList::add, ArrayList::addAll), new DummyButton(new ItemStack(Material.STAINED_GLASS_PANE)));
-        mask1.init();
-        MultiUserMultiSlotsMask mask2 = new MultiUserMultiSlotsMask("mask2", MaskUtils.generateAreaSlots(1, 2, 7, 4).collect(ArrayList::new, ArrayList::add, ArrayList::addAll), Button.EMPTY);
-        mask2.init();
 
-        Button button1 = new SimpleButton(new ItemStack(Material.GLASS_BOTTLE), (uuid, event) -> mask2.setButtons(uuid, new AirButton((uuid1, event1) -> event1.getWhoClicked().sendMessage("I'm invisible"))));
-        Button button2 = new SimpleButton(new ItemStack(Material.DIAMOND_SWORD), (uuid, event) -> mask2.setButtons(uuid, new DummyButton(new ItemStack(Material.DIAMOND_SWORD))));
-        Button animatedButton = new AnimatedButton(this, 20, true, new AirButton((uuid, event) -> {}), new DummyButton(new ItemStack(Material.GLASS)), new DummyButton(new ItemStack(Material.REDSTONE)));
-        animatedButton.init();
-        Button button3 = new SimpleButton(new ItemStack(Material.DIRT), (uuid, event) -> mask2.setButtons(uuid, animatedButton));
+        PaginatedMask mask = new PaginatedMask("pagemask", MaskUtils.generateAreaSlots(0, 0, 8, 4).collect(ArrayList::new, ArrayList::add, ArrayList::addAll));
+        for (Material material : Material.values()) {
+            if (material.isItem() && !material.isAir()) {
+                mask.addButtons(new SimpleButton(new ItemStack(material), (uuid, inventoryClickEvent) -> inventoryClickEvent.getWhoClicked().sendMessage(material.name())));
+            }
+        }
+        mask.init();
 
-        holder.addMask(mask1);
-        holder.addMask(mask2);
-        holder.addMask(new SingleMask("button1", 0, button1));
-        holder.addMask(new SingleMask("button2", 1, button2));
-        holder.addMask(new SingleMask("button3", 2, button3));
+        SingleMask nextMask = new SingleMask("nextmask", Position2D.of(0, 5), new SimpleButton(new ItemStack(Material.RED_STAINED_GLASS_PANE), (uuid, inventoryClickEvent) -> mask.previousPage(uuid)));
+        SingleMask previousMask = new SingleMask("previousmask", Position2D.of(8, 5), new SimpleButton(new ItemStack(Material.GREEN_STAINED_GLASS_PANE), (uuid, inventoryClickEvent) -> mask.nextPage(uuid)));
+        nextMask.init();
+        previousMask.init();
+
+        holder.addMask(mask);
+        holder.addMask(nextMask);
+        holder.addMask(previousMask);
+
         holder.init();
-
         Bukkit.getScheduler().runTaskTimerAsynchronously(this, this.holder::update, 0, 0);
         registerCommand(new Command("guitest") {
             @Override
