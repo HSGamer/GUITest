10d3d88a3d113dcea0f382b4f8c1ce5feaad6169
hsgamer <huynhqtienvtag@gmail.com>
2021-03-20 14:39:36 +0700

    test MaskPaginatedMask

--- src/main/java/me/hsgamer/guitest/GUITest.java
+++ src/main/java/me/hsgamer/guitest/GUITest.java
@@ -5,7 +5,8 @@ import me.hsgamer.hscore.bukkit.gui.GUIListener;
 import me.hsgamer.hscore.bukkit.gui.advanced.AdvancedGUIHolder;
 import me.hsgamer.hscore.bukkit.gui.button.impl.SimpleButton;
 import me.hsgamer.hscore.bukkit.gui.mask.MaskUtils;
-import me.hsgamer.hscore.bukkit.gui.mask.impl.PaginatedMask;
+import me.hsgamer.hscore.bukkit.gui.mask.impl.ButtonPaginatedMask;
+import me.hsgamer.hscore.bukkit.gui.mask.impl.MaskPaginatedMask;
 import me.hsgamer.hscore.bukkit.gui.mask.impl.SingleMask;
 import me.hsgamer.hscore.ui.Position2D;
 import org.bukkit.Bukkit;
@@ -52,22 +53,32 @@ public final class GUITest extends BasePlugin {
     private void initHolder() {
         holder.setSize(54);
 
-        PaginatedMask mask = new PaginatedMask("pagemask", MaskUtils.generateAreaSlots(0, 0, 8, 4).collect(ArrayList::new, ArrayList::add, ArrayList::addAll));
-        for (Material material : Material.values()) {
-            if (material.isItem() && !material.isAir()) {
-                mask.addButtons(new SimpleButton(new ItemStack(material), (uuid, inventoryClickEvent) -> inventoryClickEvent.getWhoClicked().sendMessage(material.name())));
+        MaskPaginatedMask maskPaginatedMask = new MaskPaginatedMask("mask_paginated_mask");
+        for (int i = 0; i < 5; i++) {
+            ButtonPaginatedMask buttonPaginatedMask = new ButtonPaginatedMask("button_paginated_mask_" + i, MaskUtils.generateAreaSlots(1, 0, 7, 5).collect(ArrayList::new, ArrayList::add, ArrayList::addAll));
+            for (Material material : Material.values()) {
+                if (material.isItem() && !material.isAir()) {
+                    buttonPaginatedMask.addButtons(new SimpleButton(new ItemStack(material), (uuid, event) -> event.getWhoClicked().sendMessage(material.name())));
+                }
             }
+            maskPaginatedMask.addMasks(buttonPaginatedMask);
         }
-        mask.init();
-
-        SingleMask nextMask = new SingleMask("nextmask", Position2D.of(0, 5), new SimpleButton(new ItemStack(Material.RED_STAINED_GLASS_PANE), (uuid, inventoryClickEvent) -> mask.previousPage(uuid)));
-        SingleMask previousMask = new SingleMask("previousmask", Position2D.of(8, 5), new SimpleButton(new ItemStack(Material.GREEN_STAINED_GLASS_PANE), (uuid, inventoryClickEvent) -> mask.nextPage(uuid)));
-        nextMask.init();
-        previousMask.init();
-
-        holder.addMask(mask);
-        holder.addMask(nextMask);
-        holder.addMask(previousMask);
+        maskPaginatedMask.init();
+        holder.addMask(maskPaginatedMask);
+        SingleMask nextPage = new SingleMask("next_page", Position2D.of(8, 2), new SimpleButton(new ItemStack(Material.GREEN_STAINED_GLASS_PANE), (uuid, event) -> {
+            ButtonPaginatedMask paginatedMask = (ButtonPaginatedMask) maskPaginatedMask.getMasks().get(maskPaginatedMask.getPage(uuid));
+            paginatedMask.nextPage(uuid);
+        }));
+        SingleMask prevPage = new SingleMask("prev_page", Position2D.of(0, 2), new SimpleButton(new ItemStack(Material.RED_STAINED_GLASS_PANE), (uuid, event) -> {
+            ButtonPaginatedMask paginatedMask = (ButtonPaginatedMask) maskPaginatedMask.getMasks().get(maskPaginatedMask.getPage(uuid));
+            paginatedMask.previousPage(uuid);
+        }));
+        SingleMask nextMaskPage = new SingleMask("next_mask_page", Position2D.of(8, 5), new SimpleButton(new ItemStack(Material.GREEN_STAINED_GLASS_PANE), (uuid, event) -> maskPaginatedMask.nextPage(uuid)));
+        SingleMask prevMaskPage = new SingleMask("prev_mask_page", Position2D.of(0, 5), new SimpleButton(new ItemStack(Material.RED_STAINED_GLASS_PANE), (uuid, event) -> maskPaginatedMask.previousPage(uuid)));
+        holder.addMask(nextMaskPage);
+        holder.addMask(prevMaskPage);
+        holder.addMask(nextPage);
+        holder.addMask(prevPage);
 
         holder.init();
         Bukkit.getScheduler().runTaskTimerAsynchronously(this, this.holder::update, 0, 0);
