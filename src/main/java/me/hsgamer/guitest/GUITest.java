c5fa1ff229fbce7b2d330457e868212e9ffc2452
hsgamer <huynhqtienvtag@gmail.com>
2021-03-29 21:53:48 +0700

    test MultiSlotsMask and NullButton

--- src/main/java/me/hsgamer/guitest/GUITest.java
+++ src/main/java/me/hsgamer/guitest/GUITest.java
@@ -4,9 +4,11 @@ import me.hsgamer.hscore.bukkit.baseplugin.BasePlugin;
 import me.hsgamer.hscore.bukkit.gui.GUIListener;
 import me.hsgamer.hscore.bukkit.gui.advanced.AdvancedGUIHolder;
 import me.hsgamer.hscore.bukkit.gui.button.impl.DummyButton;
+import me.hsgamer.hscore.bukkit.gui.button.impl.NullButton;
 import me.hsgamer.hscore.bukkit.gui.button.impl.SimpleButton;
 import me.hsgamer.hscore.bukkit.gui.mask.MaskUtils;
 import me.hsgamer.hscore.bukkit.gui.mask.impl.ButtonPaginatedMask;
+import me.hsgamer.hscore.bukkit.gui.mask.impl.MultiSlotsMask;
 import me.hsgamer.hscore.bukkit.gui.mask.impl.SequencePaginatedMask;
 import me.hsgamer.hscore.bukkit.gui.mask.impl.SingleMask;
 import me.hsgamer.hscore.ui.Position2D;
@@ -58,14 +60,21 @@ public final class GUITest extends BasePlugin {
         ButtonPaginatedMask buttonPaginatedMask = new ButtonPaginatedMask("button_mask_2", MaskUtils.generateAreaSlots(1, 5, 7, 5).collect(ArrayList::new, ArrayList::add , ArrayList::addAll));
         for (Material material : Material.values()) {
             if (material.isItem() && !material.isAir()) {
-                sequencePaginatedMask.addButtons(new DummyButton(new ItemStack(material)));
-                buttonPaginatedMask.addButtons(new DummyButton(new ItemStack(material)));
+                SimpleButton simpleButton = new SimpleButton(new ItemStack(material), (uuid, inventoryClickEvent) -> inventoryClickEvent.getWhoClicked().sendMessage(material.name()));
+                sequencePaginatedMask.addButtons(simpleButton);
+                buttonPaginatedMask.addButtons(simpleButton);
             }
         }
         sequencePaginatedMask.setCycle(true);
         sequencePaginatedMask.init();
         buttonPaginatedMask.setCycle(true);
         buttonPaginatedMask.init();
+
+        MultiSlotsMask multiSlotsMask = new MultiSlotsMask("override_mask", MaskUtils.generateAreaSlots(1, 1, 7, 4).collect(ArrayList::new, ArrayList::add, ArrayList::addAll));
+        multiSlotsMask.addButtons(new NullButton((uuid, inventoryClickEvent) -> inventoryClickEvent.getWhoClicked().sendMessage("Hello")));
+        multiSlotsMask.addButtons(new NullButton((uuid, inventoryClickEvent) -> {}));
+        multiSlotsMask.init();
+
         SingleMask previous = new SingleMask("pre", Position2D.of(0, 5), new SimpleButton(new ItemStack(Material.RED_STAINED_GLASS_PANE), (uuid, inventoryClickEvent) -> {
             sequencePaginatedMask.previousPage(uuid);
             buttonPaginatedMask.previousPage(uuid);
@@ -77,6 +86,7 @@ public final class GUITest extends BasePlugin {
 
         holder.addMask(sequencePaginatedMask);
         holder.addMask(buttonPaginatedMask);
+        holder.addMask(multiSlotsMask);
         holder.addMask(previous);
         holder.addMask(next);
         holder.init();
